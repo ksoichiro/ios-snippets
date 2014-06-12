111d5d06ee4525de442e9b41e705bcf99b49e7a8
ksoichiro <soichiro.kashima@gmail.com>
2014-06-12 23:36:29 +0900

    Added JSON export sample.

--- app/Snippet/Snippet/SNJSONViewController.m
+++ app/Snippet/Snippet/SNJSONViewController.m
@@ -26,6 +26,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.buttonProcess addTarget:self action:@selector(didPushButtonProcess:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,5 +45,24 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)didPushButtonProcess:(id)sender
+{
+    [self process];
+}
+
+- (void)process
+{
+    NSDictionary *subDict = @{@"f": @"foo", @"g": @"bar"};
+    NSDictionary *dict = @{@"a": @"1", @"b": @"2", @"c": @"3", @"d": @YES, @"e": subDict};
+    NSString *json = [self toJSON:dict];
+    [self.textViewProcess setText:json];
+}
+
+- (NSString *)toJSON:(NSDictionary *)dict
+{
+    NSData *data = [NSJSONSerialization dataWithJSONObject:dict options:0 error:nil];
+    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+}
 
 @end
